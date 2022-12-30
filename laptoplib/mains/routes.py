b1387aaaf5b3c7381b945c20f32b9287f298ce91
shamsurlm10 <shamsur.works@gmail.com>
2022-12-31 02:56:58 +0600

    login page, explore page, rent page implemeted

--- laptoplib/mains/routes.py
+++ laptoplib/mains/routes.py
@@ -4,7 +4,19 @@ from laptoplib import app
 
 mains = Blueprint("mains", __name__)
 
+@mains.route("/login")
+def login():
+    return render_template("mains/login.html")
+
 @mains.route("/homepage")
 @mains.route("/")
 def homepage():
     return render_template("mains/homepage.html")
+
+@mains.route("/explore")
+def explore():
+    return render_template("mains/explore.html")
+
+@mains.route("/checkout")
+def checkout():
+    return render_template("mains/checkout.html")
